0f0d54cf8fc52cbe2c3234f562706d4b523708f7
Vineeth <vineeth.v.gooli@gmail.com>
2022-12-16 02:02:46 +0530

    team changes, added mockups and responsiveness

--- js/knowledgeController.js
+++ js/knowledgeController.js
@@ -4,6 +4,64 @@ app.controller("knowledgeController", ["$scope", function ($scope) {
     self.selectedNav = 1
     self.selectedNavTab = 1
 
+
+    self.arrlist = [{
+        "id": 1,
+        "name": "For Investors"
+    }, {
+        "id": 2,
+        "name": "How it works?"
+    }, {
+        "id": 3,
+        "name": "Instruments"
+    }, {
+        "id": 4,
+        "name": "Earning a return"
+    }, {
+        "id": 5,
+        "name": "Pepcorns Fee"
+    }, {
+        "id": 6,
+        "name": "Risks"
+    }];
+
+    self.arrlist2 = [{
+        "id": 1,
+        "name": "For Companies"
+    }, {
+        "id": 2,
+        "name": "Why Pepcorns?"
+    }, {
+        "id": 3,
+        "name": "How it works?"
+    }, {
+        "id": 4,
+        "name": "Who can fundraise?"
+    }, {
+        "id": 5,
+        "name": "How do we charge?"
+    }, {
+        "id": 6,
+        "name": "Campaign"
+    }]
+
+    self.getdetails = function () {
+        if (self.userselected.id == "1")
+            self.setSubMenuIndex(1)
+        else if (self.userselected.id == "2") {
+            self.setSubMenuIndex(2);
+        } else if (self.userselected.id == "3") {
+            self.setSubMenuIndex(3);
+        } else if (self.userselected.id == "4") {
+            self.setSubMenuIndex(4);
+        } else if (self.userselected.id == "5") {
+            self.setSubMenuIndex(5);
+        } else {
+            self.setSubMenuIndex(6);
+        }
+
+    }
+
     self.isSetSubMenuIndex = function (index) {
         if (self.subMenuIndex == index) {
             return true;
--- main.js
+++ main.js
@@ -33,8 +33,8 @@ $(window).on("load", function () {
 //play video when in view
 window.addEventListener('load', videoScroll);
 window.addEventListener('scroll', videoScroll);
-var vid = document.getElementByClass("vi");
-vid.playbackRate = 2;
+var vid = document.querySelector(".vi");
+vid.playbackRate = 1;
 function videoScroll() {
 
     if (document.querySelectorAll('video[autoplay]').length > 0) {
@@ -59,3 +59,64 @@ function videoScroll() {
 }
 
 
+function subscribemailboxFun() {
+    var em2 = document.querySelector('.subscribemail').value;
+    console.log(em2)
+    subscribeInv(em2)
+}
+
+function subscribeInv(email) {
+
+    if (email.length == 0) {
+        swal("Wait up...", "Please enter a valid email address to continue.", 'warning');
+    }
+    else {
+        if (validateEmail(email)) {
+
+            //window.location.replace("https://app.pepcorns.com/#!/signup");
+            //window.location("https://pep.bitnamiapp.com/founders/#!/signup")
+            send_request(JSON.stringify({ "email": email }))
+        }
+        else {
+            swal("..mmmm", "The email ID seems incorrect.Do you mind checking it once.", 'warning');
+        }
+    }
+}
+
+
+function websiteEmailCamp(email) {
+    //?referralcode=1234&emailCampaign=123rerw
+    if (email.length == 0) {
+        swal("Wait up...", "Please enter a valid email address to continue.", 'warning');
+    }
+    else {
+        if (validateEmail(email)) {
+            window.open("https://app.pepcorns.com/#!/signup?referralcode=websitecamp&emailCampaign=" + email);
+            //send_request(JSON.stringify({ "email": email}))
+        }
+        else {
+            swal("..mmmm", "The email ID seems incorrect.Do you mind checking it once.", 'warning');
+        }
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function send_request(payload) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        // debugger
+        if (xhttp.readyState === 4) {
+            console.log("Respone-", xhttp.response);
+            swal("Hi..", JSON.parse(xhttp.response).message, "success");
+        }
+
+    }
+    xhttp.open("POST", "https://devapi.pepcorns.com/api/v1/web/subscribe", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(payload);
+}
